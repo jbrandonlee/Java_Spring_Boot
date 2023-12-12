ec3fc574133c0503a8c57410fff2367c0778686c
jbrandonlee <jbrandonlee.dev@gmail.com>
2023-12-12 09:26:59 +0800

    initial project setup

---
+++ src/test/java/sg/edu/nus/iss/javaca/LeaveManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.javaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
